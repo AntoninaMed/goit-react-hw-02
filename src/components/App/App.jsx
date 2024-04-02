c6a11dc8345c4d36c64c7f277b76d266ac3f922e
AntoninaMed <ton.kraski2427@gmail.com>
2024-04-02 22:29:06 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import { Description } from "./Description/Description";
-import { Options } from "./Options/Options";
-import { Feedback } from "./Feedback/Feedback";
-import { Notification } from "./Notification/Notification";
+import { Description } from "../Description/Description";
+import { Options } from "../Options/Options";
+import { Feedback } from "../Feedback/Feedback";
+import { Notification } from "../Notification/Notification";
 import { useState, useEffect } from "react";
 
 export default function App() {
